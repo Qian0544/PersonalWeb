d09ddd7bbcbc2f7d874e6f3a4169932bdc2db638
qianqian <qzhao0544@gmail.com>
2025-12-30 11:06:16 +0800

    Make API route dynamic

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
+export const dynamic = 'force-dynamic';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 const toEmail = process.env.TO_EMAIL; // Your personal email
